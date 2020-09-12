5bb16f03ef3e647979d956b0614b9635ae01cc95
escarolin <escarolin09@hotmail.com>
2020-09-11 22:26:28 -0700

    mmm

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
